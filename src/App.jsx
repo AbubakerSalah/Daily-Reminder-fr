ea200ec50549af5c8a982cf3a057faa587908ca2
Abubaker <Abubakersalah4@gmail.com>
2025-03-25 16:03:15 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ export default function App() {
           {date}
         </div>
 
-        <div className="flex flex-col items-center gap-16">
+        <div className="flex flex-col items-center gap-20">
           <h1 className="text-3xl font-bold text-[#E6D6C1] font-english">
             Daily Reminder
           </h1>
